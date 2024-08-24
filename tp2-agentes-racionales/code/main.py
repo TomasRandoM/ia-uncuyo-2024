1225684eace6588e5052432281f411ad2ae79f86
Kilxz <tomasrando@gmail.com>
2024-08-24 06:13:54 -0300

    Upload plot.py, Update main.py
    Se agrega plot.py para poder realizar los gráficos solicitados. Además, se modifica el main.py para poder utilizar esta función.

--- tp2-agentes-racionales/code/main.py
+++ tp2-agentes-racionales/code/main.py
@@ -1,6 +1,7 @@
 from environment import *
 from reflexiveAgent import *
 from randomAgent import *
+from plot import *
 #Utilizada para copiar los environment
 import copy
 #Utilizada para generar el excel
@@ -23,27 +24,54 @@ if __name__ == "__main__":
     sheet = excelBook.active
     #Se colocan las etiquetas en la primera columna
     sheet.cell(row = 1, column = 1, value = "Tamaño")
-    sheet.cell(row = 2, column = 1, value = "Total")
+    sheet.cell(row = 2, column = 1, value = "TotalSucios")
     sheet.cell(row = 3, column = 1, value = "LimpiadosSimple")
     sheet.cell(row = 4, column = 1, value = "MovimientosSimple")
     sheet.cell(row = 5, column = 1, value = "LimpiadosAleatorio")
     sheet.cell(row = 6, column = 1, value = "MovimientosAleatorio")
 
     n = len(environments)
+    it = 0
+    results = []
+    resultsRandom = []
     for i in range(2, n + 2):
         #Se realiza una copia del environment para que el mismo entorno sea utilizado por ambos agentes.
         environment1 = copy.deepcopy(environments[i-2])
+        #Se guardan los resultados para cada entorno y dirtRate en dos listas con el formato [Entorno, porcentajeSuciedad, celdasLimpiadas, movimientosRealizados]
+        if it == 0:
+            results.append([str(environment1.sizeX)+ "x" + str(environment1.sizeX), environment1.dirtRate, 0, 0])
+            resultsRandom.append([str(environment1.sizeX)+ "x" + str(environment1.sizeX), environment1.dirtRate, 0, 0])
         agent1 = ReflexiveAgent(environments[i-2])
         agent2 = RandomAgent(environment1)
         sheet.cell(row = 1, column = i, value = str(environment1.sizeX) + " x " + str(environment1.sizeY) + " (" + str(environment1.dirtRate) + ")")
         sheet.cell(row = 2, column = i, value = environment1.dirt)
         cleaned, moved = agent1.think()
+        #Se suman las celdas limpiadas y movimientos realizados
+        results[len(results) - 1][2] += cleaned
+        results[len(results) - 1][3] += moved
         sheet.cell(row = 3, column = i, value = cleaned)
         sheet.cell(row = 4, column = i, value = moved)
         cleaned, moved = agent2.think()
+        #Se suman las celdas limpiadas y movimientos realizados
+        resultsRandom[len(results) - 1][2] += cleaned
+        resultsRandom[len(results) - 1][3] += moved
         sheet.cell(row = 5, column = i, value = cleaned)
         sheet.cell(row = 6, column = i, value = moved)
+        it += 1
+        #Cuando se llega a las 10 iteraciones, se calculan los promedios de los resultados del entorno y dirtRate actual
+        if it == 10:
+            results[len(results) - 1][2] /= 10
+            results[len(results) - 1][3] /= 10
+            resultsRandom[len(results) - 1][2] /= 10
+            resultsRandom[len(results) - 1][3] /= 10
+            it = 0
+    #Se llama a la función plotData para que realice los gráficos para cada dirtRate
+    plotData(results, resultsRandom, 0.1)
+    plotData(results, resultsRandom, 0.2)
+    plotData(results, resultsRandom, 0.4)
+    plotData(results, resultsRandom, 0.8)
+
     #Se guarda el archivo excel.
-    excelBook.save("../resultados.xlsx")
+    excelBook.save("./results.xlsx")
     
  
---
+++ tp2-agentes-racionales/code/plot.py
@@ -0,0 +1,64 @@
+import matplotlib.pyplot as plot
+
+def plotData(results, resultsRandom, dirtRate):
+    #Se crea el gráfico de celdas limpiadas
+    plot.figure(figsize=(10,6))
+    environments =["2x2", "4x4", "8x8", "16x16", "32x32", "64x64", "128x128"]
+    promCleaned, promMoved = getResults(results, dirtRate)
+    promCleanedRandom, promMovedRandom = getResults(resultsRandom, dirtRate)
+
+    for i in range(0, len(environments)):
+        #Se colocan los puntos
+        plot.scatter([i + 1], promCleaned[i], color="blue")
+        plot.scatter([i + 1], promCleanedRandom[i], color="red")
+        #Se colocan los valores en los puntos
+        plot.text(i + 1, promCleaned[i], str(promCleaned[i]), ha='right', color='blue')
+        plot.text(i + 1, promCleanedRandom[i], str(promCleanedRandom[i]), ha='right', color='red')
+
+    #Se añaden las etiquetas a los valores del eje X
+    plot.xticks(range(1, len(environments) + 1), environments)
+    #Se añade el título
+    plot.title("Celdas limpiadas por cada agente en cada entorno para el porcentaje de suciedad " + str(dirtRate))
+    #Se añade el título del eje X
+    plot.xlabel("Entornos")
+    #Se añade el título del eje Y
+    plot.ylabel("Celdas limpiadas")
+    #Se hace visible la cuadrícula en el gráfico
+    plot.grid(True)
+    #Se guarda el gráfico
+    plot.savefig("Cleaned" + str(dirtRate) + ".png")
+    
+    #Se crea el gráfico de movimientos realizados
+    plot.figure(figsize=(10,6))
+    for i in range(0, len(environments)):
+        #Se colocan los puntos
+        plot.scatter([i + 1], promMoved[i], color="blue")
+        plot.scatter([i + 1], promMovedRandom[i], color="red")
+        #Se colocan los valores en los puntos
+        plot.text(i + 1, promMoved[i], str(promMoved[i]), ha='right', color='blue')
+        plot.text(i + 1, promMovedRandom[i], str(promMovedRandom[i]), ha='right', color='red')
+
+    #Se añaden las etiquetas a los valores del eje X
+    plot.xticks(range(1, len(environments) + 1), environments)
+    #Se añade el título
+    plot.title("Movimientos realizados hasta limpiar todo por cada agente en cada entorno para el porcentaje de suciedad " + str(dirtRate))
+    #Se añade el título del eje X
+    plot.xlabel("Entornos")
+    #Se añade el título del eje Y
+    plot.ylabel("Movimientos")
+    #Se hace visible la cuadrícula en el gráfico
+    plot.grid(True)
+    #Se guarda el gráfico
+    plot.savefig("Moved" + str(dirtRate) + ".png")
+    return 0
+
+#Divide los resultados en dos listas, una del promedio de celdas limpiadas y otra del promedio de movimientos realizados. Ambos promedios correspondientes al dirtRate especificado.
+def getResults(results, dirtRate):
+    n = len(results)
+    promCleaned = []
+    promMoved = []
+    for i in range(0, n):
+        if results[i][1] == dirtRate:
+            promCleaned.append(results[i][2])
+            promMoved.append(results[i][3])
+    return promCleaned, promMoved
